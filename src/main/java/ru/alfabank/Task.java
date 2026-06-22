34418562da4c444f77c3b24f1fc2a9372e017c00
Alina Gainulina <alina.gajnullina2015@yandex.ru>
2026-06-22 15:02:00 +0500

    Класс Task выполнен

--- src/main/java/ru/alfabank/Task.java
+++ src/main/java/ru/alfabank/Task.java
@@ -1,5 +1,31 @@
 package ru.alfabank;
 
 public class Task {
+    private String nameTask;
+    private boolean statusTask;
 
+    public String getNameTask() {
+        return nameTask;
+    }
+
+    public void setNameTask(String nameTask) {
+        this.nameTask = nameTask;
+    }
+
+    public boolean getStatusTask() {
+        return statusTask;
+    }
+
+    public void setStatusTask() {
+        this.statusTask = statusTask;
+    }
+
+    @Override
+    public String toString() {
+        String infoTask;
+        if(getStatusTask())
+            infoTask = "[x] " + getNameTask();
+        else infoTask = "[ ] " + getNameTask();
+        return infoTask;
+    }
 }
